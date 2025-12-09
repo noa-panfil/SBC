c3481bedc123a6d7174ecc4612bcf22c978ca80f
OyunOn200Hz <panfil.noa@gmail.com>
2025-12-09 23:13:18 +0100

    init du site

---
+++ tailwind-config.js
@@ -0,0 +1,17 @@
+// tailwind-config.js
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                sbc: {
+                    light: '#4ade80',  // Vert clair
+                    DEFAULT: '#15803d', // Vert principal (Seclin)
+                    dark: '#14532d',    // Vert foncé
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'], // Exemple si tu veux changer la police
+            }
+        }
+    }
+}
